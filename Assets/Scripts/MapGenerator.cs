1853428387eee16e96afa20156e2a5302e528e01
missreyyo <reyhanahsen7@gmail.com>
2023-01-14 12:14:17 +0300

    Nevmesh mask of map is completed

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,12 +7,20 @@ public class MapGenerator : MonoBehaviour
 {
     public Transform tilePrefab;
     public Transform obstaclePrefab;
+    public Transform navMeshFloor;
+    public Transform navMeshMaskPrefab;
     public Vector2 mapSize;
+    public Vector2 maxMapSize;
     [Range(0,1)]
     public float outlinePercent;
+    [Range(0,1)]
+    public float obstaclePercent;
+
+    public float tileSize = 1;
     List<Coord> allTileCoords;
     Queue<Coord> shuffledTileCoords;
     public int seed = 10;
+    Coord mapCentre;
     void Start(){
         GenerateMap();
     }
@@ -24,31 +32,87 @@ public class MapGenerator : MonoBehaviour
             }
           }
           shuffledTileCoords = new Queue<Coord>(Utility.ShuffleArray(allTileCoords.ToArray(),seed));
+          mapCentre = new Coord((int)mapSize.x/2,(int)mapSize.y/2);
          string holderName = "Generated Map";
         if(transform.FindChild (holderName)) {
             DestroyImmediate(transform.FindChild(holderName).gameObject);
         }
         Transform mapHolder = new GameObject (holderName).transform;
         mapHolder.parent = transform;
-
         for (int x = 0; x < mapSize.x; x++){
             for (int y = 0; y < mapSize.y; y++){
                 Vector3 tilePosition = CoordToPosition(x,y);
                 Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right*90)) as Transform;
-                newTile.localScale = Vector3.one * (1-outlinePercent);
+                newTile.localScale = Vector3.one * (1-outlinePercent) * tileSize;
                 newTile.parent = mapHolder;
         }
         }
-        int obstacleCount = 10;
+        bool[,] obstacleMap = new bool[(int)mapSize.x, (int)mapSize.y];
+        int obstacleCount = (int) (mapSize.x * mapSize.y * obstaclePercent);
+        int currentObstacleCount = 0;
         for (int i = 0; i <obstacleCount;i++){
             Coord randomCoord = GetRandomCoord();
+            obstacleMap[randomCoord.x, randomCoord.y] = true;
+            currentObstacleCount++;
+            if(randomCoord != mapCentre  && MapisFullyAccesible(obstacleMap, currentObstacleCount )){
             Vector3 obstaclePosition = CoordToPosition(randomCoord.x,randomCoord.y);
+            
             Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * .5f, Quaternion.identity) as Transform;
             newObstacle.parent = mapHolder;
+            newObstacle.localScale = Vector3.one * (1-outlinePercent) * tileSize;
         }
+        else{
+            obstacleMap[randomCoord.x, randomCoord.y] = false;
+            currentObstacleCount --;
+        }
+        }
+        Transform maskLeft = Instantiate(navMeshMaskPrefab, Vector3.left * (mapSize.x + maxMapSize.x) / 4 * tileSize, Quaternion.identity) as Transform; 
+        maskLeft.parent = mapHolder;  
+        maskLeft.localScale = new Vector3((maxMapSize.x - mapSize.x)/2, 1, mapSize.y) * tileSize;
+
+        Transform maskRight = Instantiate(navMeshMaskPrefab, Vector3.right * (mapSize.x + maxMapSize.x) / 4 * tileSize, Quaternion.identity) as Transform; 
+        maskRight.parent = mapHolder;  
+        maskRight.localScale = new Vector3((maxMapSize.x - mapSize.x)/2, 1, mapSize.y) * tileSize; 
+
+        Transform maskTop = Instantiate(navMeshMaskPrefab, Vector3.forward * (mapSize.x + maxMapSize.x) / 4 * tileSize, Quaternion.identity) as Transform; 
+        maskTop.parent = mapHolder;  
+        maskTop.localScale = new Vector3(maxMapSize.x, 1, (maxMapSize.y - mapSize.y)/2) * tileSize;     
+        
+        Transform maskBottom = Instantiate(navMeshMaskPrefab, Vector3.back * (mapSize.x + maxMapSize.x) / 4 * tileSize, Quaternion.identity) as Transform; 
+        maskBottom.parent = mapHolder;  
+        maskBottom.localScale = new Vector3(maxMapSize.x, 1, (maxMapSize.y - mapSize.y)/2) * tileSize;           
+
+        navMeshFloor.localScale = new Vector3(maxMapSize.x, maxMapSize.y) * tileSize;
+    }
+    bool MapisFullyAccesible(bool[,] obstacleMap, int currentObstacleCount){
+          bool[,] mapFlags = new bool[obstacleMap.GetLength(0),obstacleMap.GetLength(1)];
+          Queue<Coord> queue = new Queue<Coord> ();
+          queue.Enqueue(mapCentre);
+          mapFlags[mapCentre.x,mapCentre.y] = true;
+          int accessibleTileCount = 1;
+          while(queue.Count > 0){
+            Coord tile = queue.Dequeue();
+            for(int x = -1; x <= 1; x++){
+                for(int y = -1; y <= 1; y++){
+                   int neighbourX = tile.x + x;
+                   int neighbourY = tile.y + y;
+                   if(x == 0 || y==0){
+                      if(neighbourX >= 0 && neighbourX < obstacleMap.GetLength(0) && neighbourY >= 0 && neighbourY < obstacleMap.GetLength(1) ){
+                        if(!mapFlags[neighbourX,neighbourY] && !obstacleMap[neighbourX,neighbourY]){
+                            mapFlags[neighbourX,neighbourY] = true;
+                            queue.Enqueue(new Coord(neighbourX,neighbourY));
+                            accessibleTileCount ++;
+                        }
+                      }
+                   }
+            }
+            }
+          }
+          int targetAccessibleTileCount = (int) (mapSize.x * mapSize.y -currentObstacleCount);
+          return targetAccessibleTileCount == accessibleTileCount;
     }
     Vector3 CoordToPosition(int x, int y){
-        return new Vector3 (-mapSize.x / 2 + 0.5f + x, 0,-mapSize.y / 2 + 0.5f + y);
+        return new Vector3 (-mapSize.x / 2 + 0.5f + x, 0,-mapSize.y / 2 + 0.5f + y) * tileSize;
     }
     public Coord GetRandomCoord(){
         Coord randomCoord = shuffledTileCoords.Dequeue ();
@@ -61,6 +125,12 @@ public class MapGenerator : MonoBehaviour
         public Coord(int _x, int _y){
             x = _x;
             y = _y;
+        }
+        public static bool operator ==(Coord c1, Coord c2){
+            return c1.x == c2.x && c1.y == c2.y ;
+        }
+          public static bool operator !=(Coord c1, Coord c2){
+            return !(c1 == c2) ;
         }
     }
 }
